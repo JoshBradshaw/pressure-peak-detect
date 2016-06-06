c64f3964916ae04c808580d07c4649bf75af23de
Josh Bradshaw <joshbradshaw11@gmail.com>
2016-06-06 10:10:51 -0400

    updated script to have backtracing capability

--- pressure_peak_detect.py
+++ pressure_peak_detect.py
@@ -3,7 +3,8 @@ import collections
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-from scipy.signal import butter, lfilter
+from scipy.signal import butter, filtfilt
+import warnings
 
 """
 Algorithm adapted from:
@@ -11,49 +12,50 @@ An Open-source Algorithm to Detect Onset of Arterial Blood Pressure Pulses
 by: W Zong, T Heldt, GB Moody, RG Mark 
 """
 ### USER TUNABLE FILTER SETTINGS
-SAMPLING_RATE = 62 # Hz !!! MUST BE SET CORRECTLY
-LOWPASS_FILTER_CUTTOFF_FREQUENCY = 14 # Hz -- must be << SAMPLING_RATE / 2
-LOWPASS_FILTER_ORDER = 2 # 2 is recommended by Zong et. al as a starting point
+# Hz !!! MUST BE SET CORRECTLY FOR TIME OUTPUT TO BE ACCURATE
+# otherwise setting it approximately will be sufficient for sample rates >50Hz
+SAMPLING_RATE = 561
+MAX_EXPECTED_HR = 320 # BPM -- used to set the refractory period
+
+LOWPASS_FILTER_CUTTOFF_FREQUENCY = 16 # Hz
+# effective filtering will be 2x this order, because the filter is applied twice
+# see http://docs.scipy.org/doc/scipy-0.17.0/reference/generated/scipy.signal.filtfilt.html
+LOWPASS_FILTER_ORDER = 1  
 
 ### USER TUNABLE WAVEFORM TRANSFORM SETTINGS
+# this value is used to determine the SSF window length. This paramater is not
+# very sensitive and does not typically require adjustment
+PULSE_RISE_TIME = 0.06 # seconds
 # 128 ms window recommended by Zong et. Al for Adult BP. It is sometimes 
 # necessary to shorten this window length when dealing with very fast heartrates
-SSF_WINDOW_LENGTH = int(math.floor(SAMPLING_RATE * 0.128))
+SSF_WINDOW_LENGTH = int(math.floor(SAMPLING_RATE * PULSE_RISE_TIME))
 
 ### USER TUNABLE PEAK THRESHOLDING SETTINGS
 # BPM -- used to calculate the refractory period
 # you must use a much higher value is the subject has arrythmia or skip-beats
-MAX_EXPECTED_HR = 320 # BPM
 REFRACTORY_PERIOD = int(math.floor((60 / MAX_EXPECTED_HR) * SAMPLING_RATE))
 
 # the peak threshold is initialized as ~2-4 times the mean SSF signal value,
 # then dyanamically updated as peaks are detected
-INITIAL_PEAK_THRESHOLD = 3 # arbitrary coefficient -- should be 1-5
-
-# this value determines how many seconds of recording the initial ssf peak
-# threshold is based on. Typically 10 seconds works well, but may be too
-# long for some very short recordings
-INIT_PEAK_THRESHOLD_TIME = 10 # seconds
+INITIAL_PEAK_THRESHOLD = 2 # arbitrary coefficient -- should be 1-5
 
 # to avoid false positive detections of peaks, only detect a peaks
 # that exceed this fraction of the magnitude of the last 5 detected peaks 
-PEAK_THRESHOLD_FRACTION = 0.75 # 0-1
+PEAK_THRESHOLD_FRACTION = 0.6 # 0-1
 
 # number of peaks to average the peak threshold value over. A higher number
 # will decrease the risk of false positive detections, but increase the chance
 # of false negative (misses) detections in the event of a sudden baseline change
-PEAK_BUFFER_LEN = 5 # number of peaks
-
-# ms -- a global trigger shift to compensate for the fact that pressure
-# pulses lag the R-waves of the ECG signal. The R-wave location is a more 
-# desirable gating window beginning point than the pressure peak location
-# for most MRI reconstruction applications
-R_WAVE_DELAY_IN_SAMPLES = 8
-print R_WAVE_DELAY_IN_SAMPLES
+PEAK_BUFFER_LEN = 3 # number of peaks
 
 # the stability of the rolling point peak detection algorithm is improved
 # marginally if a larger rolling point spacing is used.
 ROLLING_POINT_SPACING = 4 # samples
+
+# seek up to 1 second back to find the start of the pressure peak
+MAXIMUM_TROUGH_SEEK_SAMPLES = SAMPLING_RATE
+
+DEBUG = True # plot the algorithm output
 
 def butter_lowpass(highcut, fs, order=5):
     """generates the filter coefficients for a butterworth lowpass filter,
@@ -71,7 +73,8 @@ def butter_lowpass_filter(data, highcut, fs, order=5):
     on the output, which may need to be accounted for in the R_WAVE_DELAY setting
     """
     b, a = butter_lowpass(highcut, fs, order=order)
-    y = lfilter(b, a, data)
+    # using filtfilt makes this a zero phase filter
+    y = filtfilt(b, a, data)
     return y
 
 def ssf(x0, x1):
@@ -86,15 +89,19 @@ def windowed_slope_sum_function(fitered_bp_vector, window_length):
     An Open-source Algorithm to Detect Onset of Arterial Blood Pressure Pulses    
     """
     # this could be replaced with simple pointer manipulation, but this is easier
+    # init with a monotonically increasing function    
     window_ring_buffer = collections.deque(np.zeros(window_length), maxlen=window_length)
 
     ssf_vector = []
     window_ssf = 0
-    for bp_sample in fitered_bp_vector:
+    for ii, bp_sample in enumerate(fitered_bp_vector):
+        window_ssf -= ssf(window_ring_buffer[0], window_ring_buffer[1])
+        window_ssf += ssf(window_ring_buffer[-1], bp_sample)
         window_ring_buffer.append(bp_sample)
-        window_ssf += ssf(window_ring_buffer[-1], window_ring_buffer[-2])
-        window_ssf -= ssf(window_ring_buffer[1], window_ring_buffer[0])
-        ssf_vector.append(window_ssf)
+        if ii > window_length:
+            ssf_vector.append(window_ssf)
+        else:
+            ssf_vector.append(0)
     return ssf_vector
     
 class PeakThreshold(object):    
@@ -103,10 +110,9 @@ class PeakThreshold(object):
     peak values.      
     """    
     def __init__(self, ssf_vector, sampling_rate):
-        samples_to_avg = INIT_PEAK_THRESHOLD_TIME * sampling_rate
-        # takes the mean of the first INIT_PEAK_THRESHOLD_TIME seconds of the SSF waveform
+        # takes the mean of the SSF waveform
         # assumes that the SSF waveform is very bottom heavy ie. mean << max
-        initial_threshold = INITIAL_PEAK_THRESHOLD * np.mean(ssf_vector[:samples_to_avg])
+        initial_threshold = INITIAL_PEAK_THRESHOLD * np.mean(ssf_vector)
         init_peak_buffer = [initial_threshold for _ in xrange(PEAK_BUFFER_LEN)]
         self.threshold_sum = PEAK_BUFFER_LEN * initial_threshold
         self.peak_buffer = collections.deque(init_peak_buffer, maxlen=PEAK_BUFFER_LEN)  
@@ -119,28 +125,19 @@ class PeakThreshold(object):
         self.peak_buffer.append(new_peak_value)
         self.threshold_sum += new_peak_value
         return self.get_threshold()
-        
-def test_peak_threshold():
-    pt = PeakThreshold(np.ones(10000), 250)
-    print pt.get_threshold()
-    print pt.update(5)
-    print pt.update(5)
 
-def estimate_all_R_wave_locations(bp_vector, sampling_rate, output_type='time'):    
+def estimate_all_pressure_peak_locations(bp_vector, sampling_rate, backtrack_to_pulse_onset=False, output_type='sample_idx'):    
     """returns a list of the R wave locations, in samples
     
     output_types:
         sample_idx -- a list of the bp_vector sample indexes which correspond to R-wave locations
         time -- the times of the R wave locations in seconds, relative to the start of bp_vector
     """
-    x = np.linspace(0, len(bp_vector)/sampling_rate, len(bp_vector))
-    R_wave_locations = []  
-    plt.plot(x, bp_vector)
+    detected_locations = []  
     lp_filtered_vector = butter_lowpass_filter(bp_vector, LOWPASS_FILTER_CUTTOFF_FREQUENCY, 
                               sampling_rate, order=LOWPASS_FILTER_ORDER)
               
     ssf_transformed_vector = windowed_slope_sum_function(lp_filtered_vector, SSF_WINDOW_LENGTH)
-    
     peak_thresholder = PeakThreshold(ssf_transformed_vector, sampling_rate)
     p_threshold = peak_thresholder.get_threshold()
     rolling_point_buffer = collections.deque(np.zeros(ROLLING_POINT_SPACING), maxlen=ROLLING_POINT_SPACING)  
@@ -148,36 +145,97 @@ def estimate_all_R_wave_locations(bp_vector, sampling_rate, output_type='time'):
     rising_edge = False
     
     # peak detection state machine
-    # criteria for a peak detection:
-    # 1. slope is trending upwards
-    # 2. refractory period since last peak detection is over
     for sample_num, bp_val in enumerate(ssf_transformed_vector):
         lrp = rolling_point_buffer[0] # left rolling point
         rrp = bp_val # right rolling point
                 
-        if rising_edge and lrp > rrp and lrp > p_threshold:
+        if rising_edge and lrp > rrp:
             r_period_count = 0
             peak_thresholder.update(lrp)
             p_threshold = peak_thresholder.get_threshold()
-            R_wave_locations.append(sample_num - R_WAVE_DELAY_IN_SAMPLES - ROLLING_POINT_SPACING)
             rising_edge = False
-        elif not rising_edge and r_period_count > REFRACTORY_PERIOD and lrp > rrp:
+            
+            if backtrack_to_pulse_onset:
+                detection_sample_num = find_onset_of_trough(lp_filtered_vector, sample_num, ssf_transformed_vector, p_threshold, sampling_rate)
+            else:
+                detection_sample_num = sample_num - ROLLING_POINT_SPACING
+            # detection_sample_num is None if find_onset_of_trough backtracking fails
+            if detection_sample_num is not None:
+                detected_locations.append(detection_sample_num)
+        elif not rising_edge and r_period_count > REFRACTORY_PERIOD and rrp > lrp and lrp > p_threshold:
             rising_edge = True
         else:
-            pass
-            
+            pass # state unchanged during this step
+        
         r_period_count += 1
         rolling_point_buffer.append(bp_val)
+    
+    # edge case where the recording ends on a rising edge, but cuts off before the peak
+    if backtrack_to_pulse_onset and rising_edge:
+        find_onset_of_trough(lp_filtered_vector, sample_num, ssf_transformed_vector, p_threshold, sampling_rate)
         
+    if DEBUG:
+        # display the intermediate filtered and SSF waveforms, for testing
+        x = np.linspace(0, len(bp_vector), len(bp_vector))
+        plt.figure(1)
+        plt.plot(x, bp_vector)
+        x = np.arange(0, len(bp_vector))
+        plt.plot(x, lp_filtered_vector)
+        plt.plot(x[:len(ssf_transformed_vector)], ssf_transformed_vector)
+        
+        bp_min = min(bp_vector)
+        bp_max = max(bp_vector)
+        detections_x = []
+        detections_y = []
+        for sample_num in detected_locations:
+            detections_x.append(sample_num)
+            detections_y.append(bp_vector[sample_num])
+        plt.title("BP, FILTERED BP, SSF TRANSFORMED BP, AND DETECTIONS")
+        plt.ylabel("Voltage, or mmHg, or whatever BP was measured in")
+        plt.xlabel("Samples")
+        plt.plot(detections_x, detections_y, 'ro')
+    
     if output_type=='sample_idx':
-        return R_wave_locations
+        return detected_locations
     elif output_type=='time':
-        R_wave_times = np.array(R_wave_locations) / sampling_rate
-        plt.plot(R_wave_times, np.ones(len(R_wave_times))*2, 'go') 
-        return R_wave_times
+        return np.array(detected_locations) / sampling_rate
     else:
         error_msg = "output_type: '{}' is invalid".format(output_type)
         raise ValueError(error_msg)
+        
+def find_onset_of_trough(lp_filtered_vector, pressure_peak_sample_num, ssf_vector, ssf_threshold, sampling_rate):
+    """
+    given the location of a pressure peak, searches backwards along the rising
+    edge to find the rightmost edge of the trough between peaks    
+    """
+    trough_onset_locations = []            
+
+    backwards_seek_range = reversed(xrange(pressure_peak_sample_num-MAXIMUM_TROUGH_SEEK_SAMPLES, pressure_peak_sample_num))
+    rolling_point_buffer = collections.deque(np.zeros(ROLLING_POINT_SPACING), maxlen=ROLLING_POINT_SPACING)    
+    rising_edge = False
+    
+    for ii in backwards_seek_range:
+        if ii < 0:
+            msg = "Unable to locate the beginning of the first pressure peak, algorithm reached start of recording"
+            warnings.warn(msg)
+            return None
+        
+        rrp = rolling_point_buffer[0] # left rolling point
+        lrp = lp_filtered_vector[ii]  # right rolling point
+        rolling_point_buffer.append(lrp)
+        
+        if rising_edge and lrp > rrp:
+            pressure_peak_location = ii + ROLLING_POINT_SPACING
+            trough_onset_locations.append(ii)
+            return pressure_peak_location
+        elif not rising_edge and lrp < rrp and ssf_vector[ii] < ssf_threshold:
+            rising_edge = True
+        else:
+            pass
+        
+    msg = "Unable to find the start of the pressure peak at time: '{}'".format(pressure_peak_sample_num / sampling_rate)
+    warnings.warn(msg)
+    return None
 
 # TUNING / TEST METHODS
 def filter_frequency_response_tester():
@@ -191,7 +249,7 @@ def filter_frequency_response_tester():
     highcut = 16
 
     # Plot the frequency response for a few different orders.
-    plt.figure(1)
+    plt.figure(2)
     plt.clf()
     for order in [3, 6, 9]:
         b, a = butter_lowpass(highcut, fs, order=order)
@@ -207,14 +265,23 @@ def filter_frequency_response_tester():
     plt.show()
 
 def test():
-    signal = []    
-    with open('yorkshire-pig-trial1.log') as f:
-        f.readline() # skip header
-        for line in f:
-            if line:
-                signal_voltage = line.split(' ')[1]
-                signal.append(float(signal_voltage))
-    estimate_all_R_wave_locations(signal, SAMPLING_RATE)
+    signal = []
+    skip_num = 1 #sometimes there is an abrupt jump at the first sample
+    us_trace = np.load("trace_IMG_20160510_1_12_anon.dcm.npy")
+    signal = us_trace[0][1][skip_num:]
+    signal_time = us_trace[0][0][skip_num:]
+	
+    print "sampling rate is 1 sample per %f seconds = %f samples per second" % (signal_time[1] - signal_time[0], 1/(signal_time[1] - signal_time[0]))
+	
+    if False:
+        plt.figure(1)
+        plt.clf()
+        plt.plot(signal_time, signal)
+
+    L = estimate_all_pressure_peak_locations(signal, SAMPLING_RATE, True, 'time')
+    print "R-wave time points = %s" % L
+    
+    plt.show()
 
 if __name__ == '__main__':
     test()
